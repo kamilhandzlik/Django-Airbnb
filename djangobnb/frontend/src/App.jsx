c5fe2e64e2424615b191ac6bf0f3d4d0c67c2cae
kamol0209 <kamol0209@gmail.com>
2025-03-20 12:03:54 +0100

    added a lot of content to frontend mainly search bar

--- djangobnb/frontend/src/App.jsx
+++ djangobnb/frontend/src/App.jsx
@@ -1,17 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import { useState } from 'react'
 import './App.css'
 import Navbar from "./components/navbar/Navbar";
+import './App.css'
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <Router>
       <Navbar/>
-      <h1>Django Bnb</h1>
-      <h2 className="dark-red-text">React and Vite are working!</h2>
-    </>
+      <div className='pt-24'>
+        <h1>Django Bnb</h1>
+        <h2 className="dark-red-text">React and Vite are working!</h2>
+      </div>
+    </Router>
   )
 }
 
--- djangobnb/frontend/src/components/navbar/Navbar.jsx
+++ djangobnb/frontend/src/components/navbar/Navbar.jsx
@@ -1,7 +1,28 @@
+import React from 'react';
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png";
+import Box from '@mui/material/Box';
+import SearchFilters from './SearchFilters';
+
+
 const Navbar = () => {
     return (
-        <nav>
-            Test
+        <nav className="navbar">
+            <div className="navbar__container">
+                <div className="navbar__logo">
+                    <Link to="/" className="navbar__logo-link" >
+                        <Box component="img" src={logo} alt="Djangobnb-logo" sx={{ width:130, height: 40 }} />
+                    </Link>
+
+                    <div className='navbar__search__field'>
+                        <SearchFilters/>
+                    </div>
+                    <div className='navbar__user_nav_and_add_property'>
+                        Add property - User nav
+                    </div>
+                </div>
+            </div>
         </nav>
     );
 }
