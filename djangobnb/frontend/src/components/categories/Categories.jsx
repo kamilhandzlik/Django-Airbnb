d090ddc5a0951965fce6ff013d064dee6ca67c18
kamol0209 <kamol0209@gmail.com>
2025-03-24 09:50:53 +0100

    finished categories

--- djangobnb/frontend/src/components/categories/Categories.jsx
+++ djangobnb/frontend/src/components/categories/Categories.jsx
@@ -1,17 +1,80 @@
 import React from "react";
 import "./Categories.css";
-import { Image } from "lucide-react";
 import Amazing from "../../assets/c5a4f6fc-c92c-4ae8-87dd-57f1ff1b89a6.jpg";
+import SwimmingPools from "../../assets/3fb523a0-b622-4368-8142-b5e03df7549b.jpg";
+import Lakeside from "../../assets/677a041d-7264-4c45-bb72-52bff21eb6e8.jpg";
+import Smallhouses from "../../assets/3271df99-f071-4ecf-9128-eb2d2b1f50f0.jpg";
+import BeautifulViews from "../../assets/3b1eb541-46d9-4bef-abc4-c37d77e3c21b.jpg";
+import Skislope from "../../assets/757deeaa-c78f-488f-992b-d3b1ecc06fc9.jpg";
+import DomeHouses from "../../assets/89faf9ae-bbbc-4bc4-aecd-cc15bf36cbca.jpg";
+import TreeHouses from "../../assets/4d4a4eba-c7e4-43eb-9ce2-95e1d200d10e.jpg";
 
 const Categories = () => {
   return (
     <main className="categories">
       <div className="categories__secondary">
-        <Image
+        <img
           className="categories__image"
           src={Amazing}
           alt="Category-Amazing"
         />
+        Amazing
+      </div>
+      <div className="categories__secondary">
+        <img
+          className="categories__image"
+          src={SwimmingPools}
+          alt="Category-Amazing"
+        />
+        Swimming pools
+      </div>
+      <div className="categories__secondary">
+        <img
+          className="categories__image"
+          src={Lakeside}
+          alt="Category-Amazing"
+        />
+        Lakeside
+      </div>
+      <div className="categories__secondary">
+        <img
+          className="categories__image"
+          src={Smallhouses}
+          alt="Category-Amazing"
+        />
+        Small Houses
+      </div>
+      <div className="categories__secondary">
+        <img
+          className="categories__image"
+          src={BeautifulViews}
+          alt="Category-Amazing"
+        />
+        Beautiful Views
+      </div>
+      <div className="categories__secondary">
+        <img
+          className="categories__image"
+          src={Skislope}
+          alt="Category-Amazing"
+        />
+        Ski Slope
+      </div>
+      <div className="categories__secondary">
+        <img
+          className="categories__image"
+          src={DomeHouses}
+          alt="Category-Amazing"
+        />
+        Dome Houses
+      </div>
+      <div className="categories__secondary">
+        <img
+          className="categories__image"
+          src={TreeHouses}
+          alt="Category-Amazing"
+        />
+        Tree Houses
       </div>
     </main>
   );
--- djangobnb/frontend/src/components/navbar/SearchFilters.jsx
+++ djangobnb/frontend/src/components/navbar/SearchFilters.jsx
@@ -2,41 +2,41 @@ import React from "react";
 import "./SearchFilter.css";
 
 const SearchFilters = () => {
-    return (
-        <div className="search__filter">
-            <div className="search__filter__container">
-                <div className="search__filter__field__1">
-                    <p className="search__filter__text">Where</p>
-                    <p className="search__filter__text">Wanted Location</p>
-                </div>
-            </div>
-            <div className="search__filter__container">
-                <div className="search__filter__field__2">
-                    <p className="search__filter__text">Check in</p>
-                    <p className="search__filter__text">Add dates</p>
-                </div>
-            </div>
-            <div className="search__filter__container">
-                <div className="search__filter__field__2">
-                    <p className="search__filter__text">Check out</p>
-                    <p className="search__filter__text">Add dates</p>
-                </div>
-            </div>
-            <div className="search__filter__container">
-                <div className="search__filter__field__2">
-                    <p className="search__filter__text">Who</p>
-                    <p className="search__filter__text">Add guests</p>
-                </div>
-            </div>
-            <div className="search__filter__container">
-                <div className="search__button">
-                   <div className="search__button__text">
-                       <box-icon name="search" color="#ffffff" size="1.5rem"></box-icon>
-                   </div>
-                </div>
-            </div>
+  return (
+    <div className="search__filter">
+      <div className="search__filter__container">
+        <div className="search__filter__field__1">
+          <p className="search__filter__text">Where</p>
+          <p className="search__filter__text">Wanted Location</p>
         </div>
-    );
+      </div>
+      <div className="search__filter__container">
+        <div className="search__filter__field__2">
+          <p className="search__filter__text">Check in</p>
+          <p className="search__filter__text">Add dates</p>
+        </div>
+      </div>
+      <div className="search__filter__container">
+        <div className="search__filter__field__2">
+          <p className="search__filter__text">Check out</p>
+          <p className="search__filter__text">Add dates</p>
+        </div>
+      </div>
+      <div className="search__filter__container">
+        <div className="search__filter__field__2">
+          <p className="search__filter__text">Who</p>
+          <p className="search__filter__text">Add guests</p>
+        </div>
+      </div>
+      <div className="search__filter__container">
+        <div className="search__button">
+          <div className="search__button__text">
+            <box-icon name="search" color="#ffffff" size="1.5rem"></box-icon>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SearchFilters;
