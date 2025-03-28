cbf9c8a7821742a9692fb069ac3bd079fc50e086
kamol0209 <kamol0209@gmail.com>
2025-03-28 12:19:13 +0100

    finished frontend fo my reservations

--- djangobnb/frontend/src/components/myreservations/MyReservations.jsx
+++ djangobnb/frontend/src/components/myreservations/MyReservations.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./MyReservations.css";
 import NavBar from "../navbar/Navbar";
 import Categories from "../categories/Categories";
+import BeachHouse from "../images/beach_house_1.jpg";
 
 const MyResevations = () => {
   return (
@@ -13,8 +14,102 @@ const MyResevations = () => {
       <main className="container">
         <h1 className="my-reservations-text-title">My reservations</h1>
         <div className="my-reservations-page-grid">
-          <div className="my-reservations-page-reservations">asdfa</div>
-          <div className="my-reservations-page-reservations">asdfa</div>
+          <div className="my-reservations-page-reservations">
+            <div className="my-reservations-row">
+              <div className="property-image">
+                <img
+                  src={BeachHouse}
+                  alt="beach-house"
+                  className="property-image"
+                />
+              </div>
+              <div>
+                <div className="my-reservations-text-property-name">
+                  Property Name
+                </div>
+                <div className="my-reservations-page-details">
+                  <p className="my-reservations-text-property-details">
+                    Check in date:
+                  </p>
+                  <p className="my-reservations-text-property-descriptions">
+                    14/04/2025
+                  </p>
+                </div>
+                <div className="my-reservations-page-details">
+                  <p className="my-reservations-text-property-details">
+                    Check out date:
+                  </p>
+                  <p className="my-reservations-text-property-descriptions">
+                    15/04/2025
+                  </p>
+                </div>
+                <div className="my-reservations-page-details">
+                  <p className="my-reservations-text-property-details">
+                    Number of nights:
+                  </p>
+                  <p className="my-reservations-text-property-descriptions">
+                    1
+                  </p>
+                </div>
+                <div className="my-reservations-page-details">
+                  <p className="my-reservations-text-property-details">
+                    Total price:
+                  </p>
+                  <p className="my-reservations-text-property-descriptions">
+                    $100
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="my-reservations-page-reservations">
+            <div className="my-reservations-row">
+              <div className="property-image">
+                <img
+                  src={BeachHouse}
+                  alt="beach-house"
+                  className="property-image"
+                />
+              </div>
+              <div>
+                <div className="my-reservations-text-property-name">
+                  Property Name
+                </div>
+                <div className="my-reservations-page-details">
+                  <p className="my-reservations-text-property-details">
+                    Check in date:
+                  </p>
+                  <p className="my-reservations-text-property-descriptions">
+                    14/04/2025
+                  </p>
+                </div>
+                <div className="my-reservations-page-details">
+                  <p className="my-reservations-text-property-details">
+                    Check out date:
+                  </p>
+                  <p className="my-reservations-text-property-descriptions">
+                    15/04/2025
+                  </p>
+                </div>
+                <div className="my-reservations-page-details">
+                  <p className="my-reservations-text-property-details">
+                    Number of nights:
+                  </p>
+                  <p className="my-reservations-text-property-descriptions">
+                    1
+                  </p>
+                </div>
+                <div className="my-reservations-page-details">
+                  <p className="my-reservations-text-property-details">
+                    Total price:
+                  </p>
+                  <p className="my-reservations-text-property-descriptions">
+                    $100
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </main>
     </>
