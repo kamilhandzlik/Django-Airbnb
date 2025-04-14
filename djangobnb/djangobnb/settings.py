09c76581635bcefaf998678245ec2e83de3d87b9
kamol0209 <kamol0209@gmail.com>
2025-04-14 11:33:13 +0200

    .

--- djangobnb/djangobnb/settings.py
+++ djangobnb/djangobnb/settings.py
@@ -66,9 +66,11 @@ REST_FRAMEWORK = {
     ],
 }
 
-CORS_ALLOW_ORIGINS = [
-    "http://127.0.0.1:8000",
+CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
+    "http://127.0.0.1:8000",
+    'http://localhost:3000',
+    'http://localhost:8000',
 ]
 
 
--- djangobnb/frontend/src/components/properties/PropertyList.jsx
+++ djangobnb/frontend/src/components/properties/PropertyList.jsx
@@ -1,10 +1,30 @@
+"use client";
+import { useEffect } from "react";
 import PropertyListItem from "./PropertyListItem";
 import React from "react";
 import "./PropertyList.css";
 
 const PropertyList = () => {
+  const getProperties = async () => {
+    console.log("Attempting to fetch properties...");
+    const url = "http://localhost:8000/api/properties/";
+    try {
+      const response = await fetch(url);
+      const json = await response.json();
+      console.log("json:", json);
+    } catch (error) {
+      console.error("Error fetching properties:", error);
+    }
+  };
+
+  useEffect(() => {
+    console.log("useEffect fired!");
+    getProperties();
+  }, []);
+
   return (
     <div className="property-grid">
+      <h2 style={{ color: "red" }}>Hello from PropertyList!</h2>
       <div className="property-item">
         <PropertyListItem />
       </div>
